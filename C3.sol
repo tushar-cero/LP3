14515ea1b06fa35a7113207b88fb1916f58cacd0
Tushar Singh <tusharsingh.contact@gmail.com>
2023-11-05 21:01:15 +0530

    Bank account smart contract

--- C3.sol
+++ C3.sol
@@ -1,23 +1,49 @@
-pragma solidity ^0.4.19;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract TipJar {
+contract BankAccount {
+    address public owner;
+    mapping(address => uint256) public balances;
 
-    address owner;
+    event Deposited(address indexed account, uint256 amount);
+    event Withdrawn(address indexed account, uint256 amount);
+    event Transferred(address indexed from, address indexed to, uint256 amount);
 
-    function TipJar() public {
+    constructor() {
         owner = msg.sender;
     }
 
-    function withdraw() public {
-        require(owner == msg.sender);
-        msg.sender.transfer(address(this).balance);
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action");
+        _;
     }
 
-    function deposit(uint256 amount) public payable {
-        require(msg.value == amount);
+    function deposit() public payable {
+        require(msg.value > 0, "Deposit amount must be greater than 0");
+        balances[msg.sender] += msg.value;
+        emit Deposited(msg.sender, msg.value);
+    }
+
+    function withdraw(uint256 amount) public {
+        require(amount > 0, "Withdrawal amount must be greater than 0");
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+        emit Withdrawn(msg.sender, amount);
     }
 
     function getBalance() public view returns (uint256) {
-        return address(this).balance;
+        return balances[msg.sender];
+    }
+
+    function transfer(address to, uint256 amount) public {
+        require(to != address(0), "Invalid recipient address");
+        require(to != msg.sender, "Cannot transfer to yourself");
+        require(balances[msg.sender] >= amount, "Insufficient balance for transfer");
+
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+
+        emit Transferred(msg.sender, to, amount);
     }
 }
